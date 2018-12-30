4afc023295b779e25189601a50c5161412c22546
Yurii Popovych <c.ypopovych@gogoair.com>
2018-12-30 15:58:01 +0200

    delete comments

--- Objects/problem_5_1_9.py
+++ Objects/problem_5_1_9.py
@@ -1,21 +1,4 @@
 
-#Make sure you name each setter with the format: 
-#"set_some_attribute" and "get_some_attribute"
-#
-#For example, the getter for meat would be get_meat. The
-#getter for to_go would be get_to_go.
-#
-#Hint: Your code is going to end up *very* long. This
-#will be the longest program you've written so far, but
-#it isn't the most complex. Complexity and length are
-#often very different!
-#
-#Hint 2: Checking for valid values will be much easier
-#if you make a list of valid values for each attribute
-#and check the new value against that.
-
-
-#Write your code here!
 class Burrito:
     
     def __init__(self, meat, to_go, rice, beans, extra_meat = False, guacamole = False, cheese = False, pico = False, corn = False ):
